69c6c833f39eb15382ba2af2f73cd057cd5d1a44
MegaWatchLTD <102382969+MegaWatchLTD@users.noreply.github.com>
2022-03-26 16:36:43 +0100

    Add files via upload

---
+++ Episodes/Ep11/server4/script.js
@@ -0,0 +1,68 @@
+const playerInstance = jwplayer("player").setup({
+  controls: true,
+  displaytitle: false,
+  fullscreen: "true",
+  primary: "html5",
+  stretching: "uniform",
+  aspectratio: "16:9",
+  renderCaptionsNatively: false,
+  autostart: false,
+  abouttext: "Creator Website",
+  aboutlink: "https://www.fb.com/ArabyMovies",
+
+  skin: {
+    name: "netflix"
+  },
+
+  logo: {
+    file: "../../../../logo.png"
+  },
+
+      playlist: [
+        ,
+        {        
+        title: "Harga Ep11",
+        description: "Harga Ep11",
+        image: "thumbnail.jpg",
+        sources: [{
+        file: "https://www.googleapis.com/drive/v3/files/1fP8-pvWLvLwOqQvqzp-bHYoixkUb_Zhb?alt=media&key=AIzaSyCkb7pLkD3PUkMLawSoto6Dm8_4tJHdmgY&v=.mp4",
+          label: '1080p',
+          'type': 'mp4',
+          primary: 'html5',
+
+        },{
+         file: "https://www.googleapis.com/drive/v3/files/1fP8-pvWLvLwOqQvqzp-bHYoixkUb_Zhb?alt=media&key=AIzaSyCkb7pLkD3PUkMLawSoto6Dm8_4tJHdmgY&v=.mp4",
+          label: '720p',
+          'type': 'mp4',
+          primary: 'html5',
+        },{
+         file: "https://www.googleapis.com/drive/v3/files/1fP8-pvWLvLwOqQvqzp-bHYoixkUb_Zhb?alt=media&key=AIzaSyCkb7pLkD3PUkMLawSoto6Dm8_4tJHdmgY&v=.mp4",
+          label: '480p',
+          'type': 'mp4',
+          primary: 'html5',
+        }],
+
+        
+      }//end of movies
+      ]
+    });
+
+playerInstance.on("ready", function () {
+  // Move the timeslider in-line with other controls
+  const playerContainer = playerInstance.getContainer();
+  const buttonContainer = playerContainer.querySelector(".jw-button-container");
+  const spacer = buttonContainer.querySelector(".jw-spacer");
+  const timeSlider = playerContainer.querySelector(".jw-slider-time");
+  buttonContainer.replaceChild(timeSlider, spacer);
+});
+
+jwplayer("container").setCaptions({
+  "back": true,
+  "backgroundOpacity": "32",
+  "edgeStyle": "dropshadow",
+  "fontSize": 14,
+  "fontOpacity": 100,
+  "fontScale": 0.05,
+  "windowOpacity": 0,
+  "color": "#ffff00"
+});
