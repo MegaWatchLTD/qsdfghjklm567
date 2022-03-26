3a78203a6f1dedd3d6f782f7ccefd3695664d32c
MegaWatchLTD <102382969+MegaWatchLTD@users.noreply.github.com>
2022-03-26 16:16:10 +0100

    Add files via upload

---
+++ js/changeurl.js
@@ -0,0 +1,259 @@
+function changeurl1(){
+   document.getElementById("Ep1-1").setAttribute("href", "../Harga/Episodes/Ep1/server1/play.html");
+   document.getElementById("Ep1-2").setAttribute("href", "../Harga/Episodes/Ep1/server1/play.html");
+
+   document.getElementById("Ep2-1").setAttribute("href", "../Harga/Episodes/Ep2/server1/play.html");
+   document.getElementById("Ep2-2").setAttribute("href", "../Harga/Episodes/Ep2/server1/play.html");
+
+   document.getElementById("Ep3-1").setAttribute("href", "../Harga/Episodes/Ep3/server1/play.html");
+   document.getElementById("Ep3-2").setAttribute("href", "../Harga/Episodes/Ep3/server1/play.html");
+
+   document.getElementById("Ep4-1").setAttribute("href", "../Harga/Episodes/Ep4/server1/play.html");
+   document.getElementById("Ep4-2").setAttribute("href", "../Harga/Episodes/Ep4/server1/play.html");
+
+   document.getElementById("Ep5-1").setAttribute("href", "../Harga/Episodes/Ep5/server1/play.html");
+   document.getElementById("Ep5-2").setAttribute("href", "../Harga/Episodes/Ep5/server1/play.html");
+
+   document.getElementById("Ep6-1").setAttribute("href", "../Harga/Episodes/Ep6/server1/play.html");    
+   document.getElementById("Ep6-2").setAttribute("href", "../Harga/Episodes/Ep6/server1/play.html");    
+
+   document.getElementById("Ep7-1").setAttribute("href", "../Harga/Episodes/Ep7/server1/play.html");
+   document.getElementById("Ep7-2").setAttribute("href", "../Harga/Episodes/Ep7/server1/play.html");
+
+   document.getElementById("Ep8-1").setAttribute("href", "../Harga/Episodes/Ep8/server1/play.html");
+   document.getElementById("Ep8-2").setAttribute("href", "../Harga/Episodes/Ep8/server1/play.html");
+
+   document.getElementById("Ep9-1").setAttribute("href", "../Harga/Episodes/Ep9/server1/play.html");    
+   document.getElementById("Ep9-2").setAttribute("href", "../Harga/Episodes/Ep9/server1/play.html");    
+
+   document.getElementById("Ep10-1").setAttribute("href", "../Harga/Episodes/Ep10/server1/play.html");
+   document.getElementById("Ep10-2").setAttribute("href", "../Harga/Episodes/Ep10/server1/play.html");
+
+   document.getElementById("Ep11-1").setAttribute("href", "../Harga/Episodes/Ep11/server1/play.html");
+   document.getElementById("Ep11-2").setAttribute("href", "../Harga/Episodes/Ep11/server1/play.html");
+
+   document.getElementById("Ep12-1").setAttribute("href", "../Harga/Episodes/Ep12/server1/play.html");    
+   document.getElementById("Ep12-2").setAttribute("href", "../Harga/Episodes/Ep12/server1/play.html");    
+
+   document.getElementById("Ep13-1").setAttribute("href", "../Harga/Episodes/Ep13/server1/play.html");
+   document.getElementById("Ep13-2").setAttribute("href", "../Harga/Episodes/Ep13/server1/play.html");
+
+   document.getElementById("Ep14-1").setAttribute("href", "../Harga/Episodes/Ep14/server1/play.html");
+   document.getElementById("Ep14-2").setAttribute("href", "../Harga/Episodes/Ep14/server1/play.html");
+
+   document.getElementById("Ep15-1").setAttribute("href", "../Harga/Episodes/Ep15/server1/play.html");    
+   document.getElementById("Ep15-2").setAttribute("href", "../Harga/Episodes/Ep15/server1/play.html");    
+
+   document.getElementById("Ep16-1").setAttribute("href", "../Harga/Episodes/Ep16/server1/play.html");
+   document.getElementById("Ep16-2").setAttribute("href", "../Harga/Episodes/Ep16/server1/play.html");
+
+   document.getElementById("Ep17-1").setAttribute("href", "../Harga/Episodes/Ep17/server1/play.html");
+   document.getElementById("Ep17-2").setAttribute("href", "../Harga/Episodes/Ep17/server1/play.html");
+
+   document.getElementById("Ep18-1").setAttribute("href", "../Harga/Episodes/Ep18/server1/play.html");    
+   document.getElementById("Ep18-2").setAttribute("href", "../Harga/Episodes/Ep18/server1/play.html");    
+
+   document.getElementById("Ep19-1").setAttribute("href", "../Harga/Episodes/Ep19/server1/play.html");        
+   document.getElementById("Ep19-2").setAttribute("href", "../Harga/Episodes/Ep19/server1/play.html");        
+
+   document.getElementById("Ep20-1").setAttribute("href", "../Harga/Episodes/Ep20/server1/play.html");
+   document.getElementById("Ep20-2").setAttribute("href", "../Harga/Episodes/Ep20/server1/play.html");
+    
+   return false;
+   }
+
+
+function changeurl2(){
+   document.getElementById("Ep1-1").setAttribute("href", "../Harga/Episodes/Ep1/server2/play.html");
+   document.getElementById("Ep1-2").setAttribute("href", "../Harga/Episodes/Ep1/server2/play.html");
+
+   document.getElementById("Ep2-1").setAttribute("href", "../Harga/Episodes/Ep2/server2/play.html");
+   document.getElementById("Ep2-2").setAttribute("href", "../Harga/Episodes/Ep2/server2/play.html");
+
+   document.getElementById("Ep3-1").setAttribute("href", "../Harga/Episodes/Ep3/server2/play.html");
+   document.getElementById("Ep3-2").setAttribute("href", "../Harga/Episodes/Ep3/server2/play.html");
+
+   document.getElementById("Ep4-1").setAttribute("href", "../Harga/Episodes/Ep4/server2/play.html");
+   document.getElementById("Ep4-2").setAttribute("href", "../Harga/Episodes/Ep4/server2/play.html");
+
+   document.getElementById("Ep5-1").setAttribute("href", "../Harga/Episodes/Ep5/server2/play.html");
+   document.getElementById("Ep5-2").setAttribute("href", "../Harga/Episodes/Ep5/server2/play.html");
+
+   document.getElementById("Ep6-1").setAttribute("href", "../Harga/Episodes/Ep6/server2/play.html");    
+   document.getElementById("Ep6-2").setAttribute("href", "../Harga/Episodes/Ep6/server2/play.html");    
+
+   document.getElementById("Ep7-1").setAttribute("href", "../Harga/Episodes/Ep7/server2/play.html");
+   document.getElementById("Ep7-2").setAttribute("href", "../Harga/Episodes/Ep7/server2/play.html");
+
+   document.getElementById("Ep8-1").setAttribute("href", "../Harga/Episodes/Ep8/server2/play.html");
+   document.getElementById("Ep8-2").setAttribute("href", "../Harga/Episodes/Ep8/server2/play.html");
+
+   document.getElementById("Ep9-1").setAttribute("href", "../Harga/Episodes/Ep9/server2/play.html");    
+   document.getElementById("Ep9-2").setAttribute("href", "../Harga/Episodes/Ep9/server2/play.html");    
+
+   document.getElementById("Ep10-1").setAttribute("href", "../Harga/Episodes/Ep10/server2/play.html");
+   document.getElementById("Ep10-2").setAttribute("href", "../Harga/Episodes/Ep10/server2/play.html");
+
+   document.getElementById("Ep11-1").setAttribute("href", "../Harga/Episodes/Ep11/server2/play.html");
+   document.getElementById("Ep11-2").setAttribute("href", "../Harga/Episodes/Ep11/server2/play.html");
+
+   document.getElementById("Ep12-1").setAttribute("href", "../Harga/Episodes/Ep12/server2/play.html");    
+   document.getElementById("Ep12-2").setAttribute("href", "../Harga/Episodes/Ep12/server2/play.html");    
+
+   document.getElementById("Ep13-1").setAttribute("href", "../Harga/Episodes/Ep13/server2/play.html");
+   document.getElementById("Ep13-2").setAttribute("href", "../Harga/Episodes/Ep13/server2/play.html");
+
+   document.getElementById("Ep14-1").setAttribute("href", "../Harga/Episodes/Ep14/server2/play.html");
+   document.getElementById("Ep14-2").setAttribute("href", "../Harga/Episodes/Ep14/server2/play.html");
+
+   document.getElementById("Ep15-1").setAttribute("href", "../Harga/Episodes/Ep15/server2/play.html");    
+   document.getElementById("Ep15-2").setAttribute("href", "../Harga/Episodes/Ep15/server2/play.html");    
+
+   document.getElementById("Ep16-1").setAttribute("href", "../Harga/Episodes/Ep16/server2/play.html");
+   document.getElementById("Ep16-2").setAttribute("href", "../Harga/Episodes/Ep16/server2/play.html");
+
+   document.getElementById("Ep17-1").setAttribute("href", "../Harga/Episodes/Ep17/server2/play.html");
+   document.getElementById("Ep17-2").setAttribute("href", "../Harga/Episodes/Ep17/server2/play.html");
+
+   document.getElementById("Ep18-1").setAttribute("href", "../Harga/Episodes/Ep18/server2/play.html");    
+   document.getElementById("Ep18-2").setAttribute("href", "../Harga/Episodes/Ep18/server2/play.html");    
+
+   document.getElementById("Ep19-1").setAttribute("href", "../Harga/Episodes/Ep19/server2/play.html");        
+   document.getElementById("Ep19-2").setAttribute("href", "../Harga/Episodes/Ep19/server2/play.html");        
+
+   document.getElementById("Ep20-1").setAttribute("href", "../Harga/Episodes/Ep20/server2/play.html");
+   document.getElementById("Ep20-2").setAttribute("href", "../Harga/Episodes/Ep20/server2/play.html");
+
+   return false;
+   }
+
+
+function changeurl3(){
+   document.getElementById("Ep1-1").setAttribute("href", "../Harga/Episodes/Ep1/server3/play.html");
+   document.getElementById("Ep1-2").setAttribute("href", "../Harga/Episodes/Ep1/server3/play.html");
+
+   document.getElementById("Ep2-1").setAttribute("href", "../Harga/Episodes/Ep2/server3/play.html");
+   document.getElementById("Ep2-2").setAttribute("href", "../Harga/Episodes/Ep2/server3/play.html");
+
+   document.getElementById("Ep3-1").setAttribute("href", "../Harga/Episodes/Ep3/server3/play.html");
+   document.getElementById("Ep3-2").setAttribute("href", "../Harga/Episodes/Ep3/server3/play.html");
+
+   document.getElementById("Ep4-1").setAttribute("href", "../Harga/Episodes/Ep4/server3/play.html");
+   document.getElementById("Ep4-2").setAttribute("href", "../Harga/Episodes/Ep4/server3/play.html");
+
+   document.getElementById("Ep5-1").setAttribute("href", "../Harga/Episodes/Ep5/server3/play.html");
+   document.getElementById("Ep5-2").setAttribute("href", "../Harga/Episodes/Ep5/server3/play.html");
+
+   document.getElementById("Ep6-1").setAttribute("href", "../Harga/Episodes/Ep6/server3/play.html");    
+   document.getElementById("Ep6-2").setAttribute("href", "../Harga/Episodes/Ep6/server3/play.html");    
+
+   document.getElementById("Ep7-1").setAttribute("href", "../Harga/Episodes/Ep7/server3/play.html");
+   document.getElementById("Ep7-2").setAttribute("href", "../Harga/Episodes/Ep7/server3/play.html");
+
+   document.getElementById("Ep8-1").setAttribute("href", "../Harga/Episodes/Ep8/server3/play.html");
+   document.getElementById("Ep8-2").setAttribute("href", "../Harga/Episodes/Ep8/server3/play.html");
+
+   document.getElementById("Ep9-1").setAttribute("href", "../Harga/Episodes/Ep9/server3/play.html");    
+   document.getElementById("Ep9-2").setAttribute("href", "../Harga/Episodes/Ep9/server3/play.html");    
+
+   document.getElementById("Ep10-1").setAttribute("href", "../Harga/Episodes/Ep10/server3/play.html");
+   document.getElementById("Ep10-2").setAttribute("href", "../Harga/Episodes/Ep10/server3/play.html");
+
+   document.getElementById("Ep11-1").setAttribute("href", "../Harga/Episodes/Ep11/server3/play.html");
+   document.getElementById("Ep11-2").setAttribute("href", "../Harga/Episodes/Ep11/server3/play.html");
+
+   document.getElementById("Ep12-1").setAttribute("href", "../Harga/Episodes/Ep12/server3/play.html");    
+   document.getElementById("Ep12-2").setAttribute("href", "../Harga/Episodes/Ep12/server3/play.html");    
+
+   document.getElementById("Ep13-1").setAttribute("href", "../Harga/Episodes/Ep13/server3/play.html");
+   document.getElementById("Ep13-2").setAttribute("href", "../Harga/Episodes/Ep13/server3/play.html");
+
+   document.getElementById("Ep14-1").setAttribute("href", "../Harga/Episodes/Ep14/server3/play.html");
+   document.getElementById("Ep14-2").setAttribute("href", "../Harga/Episodes/Ep14/server3/play.html");
+
+   document.getElementById("Ep15-1").setAttribute("href", "../Harga/Episodes/Ep15/server3/play.html");    
+   document.getElementById("Ep15-2").setAttribute("href", "../Harga/Episodes/Ep15/server3/play.html");    
+
+   document.getElementById("Ep16-1").setAttribute("href", "../Harga/Episodes/Ep16/server3/play.html");
+   document.getElementById("Ep16-2").setAttribute("href", "../Harga/Episodes/Ep16/server3/play.html");
+
+   document.getElementById("Ep17-1").setAttribute("href", "../Harga/Episodes/Ep17/server3/play.html");
+   document.getElementById("Ep17-2").setAttribute("href", "../Harga/Episodes/Ep17/server3/play.html");
+
+   document.getElementById("Ep18-1").setAttribute("href", "../Harga/Episodes/Ep18/server3/play.html");    
+   document.getElementById("Ep18-2").setAttribute("href", "../Harga/Episodes/Ep18/server3/play.html");    
+
+   document.getElementById("Ep19-1").setAttribute("href", "../Harga/Episodes/Ep19/server3/play.html");        
+   document.getElementById("Ep19-2").setAttribute("href", "../Harga/Episodes/Ep19/server3/play.html");        
+
+   document.getElementById("Ep20-1").setAttribute("href", "../Harga/Episodes/Ep20/server3/play.html");
+   document.getElementById("Ep20-2").setAttribute("href", "../Harga/Episodes/Ep20/server3/play.html");
+
+   return false;
+   }
+
+
+
+function changeurl4(){
+   document.getElementById("Ep1-1").setAttribute("href", "../Harga/Episodes/Ep1/server4/play.html");
+   document.getElementById("Ep1-2").setAttribute("href", "../Harga/Episodes/Ep1/server4/play.html");
+
+   document.getElementById("Ep2-1").setAttribute("href", "../Harga/Episodes/Ep2/server4/play.html");
+   document.getElementById("Ep2-2").setAttribute("href", "../Harga/Episodes/Ep2/server4/play.html");
+
+   document.getElementById("Ep3-1").setAttribute("href", "../Harga/Episodes/Ep3/server4/play.html");
+   document.getElementById("Ep3-2").setAttribute("href", "../Harga/Episodes/Ep3/server4/play.html");
+
+   document.getElementById("Ep4-1").setAttribute("href", "../Harga/Episodes/Ep4/server4/play.html");
+   document.getElementById("Ep4-2").setAttribute("href", "../Harga/Episodes/Ep4/server4/play.html");
+
+   document.getElementById("Ep5-1").setAttribute("href", "../Harga/Episodes/Ep5/server4/play.html");
+   document.getElementById("Ep5-2").setAttribute("href", "../Harga/Episodes/Ep5/server4/play.html");
+
+   document.getElementById("Ep6-1").setAttribute("href", "../Harga/Episodes/Ep6/server4/play.html");    
+   document.getElementById("Ep6-2").setAttribute("href", "../Harga/Episodes/Ep6/server4/play.html");    
+
+   document.getElementById("Ep7-1").setAttribute("href", "../Harga/Episodes/Ep7/server4/play.html");
+   document.getElementById("Ep7-2").setAttribute("href", "../Harga/Episodes/Ep7/server4/play.html");
+
+   document.getElementById("Ep8-1").setAttribute("href", "../Harga/Episodes/Ep8/server4/play.html");
+   document.getElementById("Ep8-2").setAttribute("href", "../Harga/Episodes/Ep8/server4/play.html");
+
+   document.getElementById("Ep9-1").setAttribute("href", "../Harga/Episodes/Ep9/server4/play.html");    
+   document.getElementById("Ep9-2").setAttribute("href", "../Harga/Episodes/Ep9/server4/play.html");    
+
+   document.getElementById("Ep10-1").setAttribute("href", "../Harga/Episodes/Ep10/server4/play.html");
+   document.getElementById("Ep10-2").setAttribute("href", "../Harga/Episodes/Ep10/server4/play.html");
+
+   document.getElementById("Ep11-1").setAttribute("href", "../Harga/Episodes/Ep11/server4/play.html");
+   document.getElementById("Ep11-2").setAttribute("href", "../Harga/Episodes/Ep11/server4/play.html");
+
+   document.getElementById("Ep12-1").setAttribute("href", "../Harga/Episodes/Ep12/server4/play.html");    
+   document.getElementById("Ep12-2").setAttribute("href", "../Harga/Episodes/Ep12/server4/play.html");    
+
+   document.getElementById("Ep13-1").setAttribute("href", "../Harga/Episodes/Ep13/server4/play.html");
+   document.getElementById("Ep13-2").setAttribute("href", "../Harga/Episodes/Ep13/server4/play.html");
+
+   document.getElementById("Ep14-1").setAttribute("href", "../Harga/Episodes/Ep14/server4/play.html");
+   document.getElementById("Ep14-2").setAttribute("href", "../Harga/Episodes/Ep14/server4/play.html");
+
+   document.getElementById("Ep15-1").setAttribute("href", "../Harga/Episodes/Ep15/server4/play.html");    
+   document.getElementById("Ep15-2").setAttribute("href", "../Harga/Episodes/Ep15/server4/play.html");    
+
+   document.getElementById("Ep16-1").setAttribute("href", "../Harga/Episodes/Ep16/server4/play.html");
+   document.getElementById("Ep16-2").setAttribute("href", "../Harga/Episodes/Ep16/server4/play.html");
+
+   document.getElementById("Ep17-1").setAttribute("href", "../Harga/Episodes/Ep17/server4/play.html");
+   document.getElementById("Ep17-2").setAttribute("href", "../Harga/Episodes/Ep17/server4/play.html");
+
+   document.getElementById("Ep18-1").setAttribute("href", "../Harga/Episodes/Ep18/server4/play.html");    
+   document.getElementById("Ep18-2").setAttribute("href", "../Harga/Episodes/Ep18/server4/play.html");    
+
+   document.getElementById("Ep19-1").setAttribute("href", "../Harga/Episodes/Ep19/server4/play.html");        
+   document.getElementById("Ep19-2").setAttribute("href", "../Harga/Episodes/Ep19/server4/play.html");        
+
+   document.getElementById("Ep20-1").setAttribute("href", "../Harga/Episodes/Ep20/server4/play.html");
+   document.getElementById("Ep20-2").setAttribute("href", "../Harga/Episodes/Ep20/server4/play.html");
+
+   return false;
+   }         
